b024d3c50c3a2b6761861dda173520f8f3e8324b
Magfoxx <hamcha.martial@gmail.com>
2024-09-19 09:39:38 +0200

    Correction bug CORS

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const books = require('../frontend/public/data/data.json');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 // Route des 3 meilleurs livres (GET)
 app.get('/api/books/bestRating', (req, res) => {
